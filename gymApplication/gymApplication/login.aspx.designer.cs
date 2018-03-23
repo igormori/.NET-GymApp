c61fd5eef3501b819c9d14d1cb8987eb7a86639f
igorMori <igor_canada@outlook.com>
2018-03-23 15:09:58 -0400

    Login done

--- gymApplication/gymApplication/login.aspx.designer.cs
+++ gymApplication/gymApplication/login.aspx.designer.cs
@@ -40,6 +40,15 @@ namespace gymApplication {
         protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
         
         /// <summary>
+        /// Label1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Label Label1;
+        
+        /// <summary>
         /// Email control.
         /// </summary>
         /// <remarks>
