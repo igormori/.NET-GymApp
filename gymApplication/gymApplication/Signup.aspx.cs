61a07491c9a1290884b1b9f1cc3d6342e87c3521
Sherwayne W <sherwaynewalrond@gmail.com>
2018-04-06 19:37:20 -0400

    Created database for signup page
    Created Users table and added gridview for testing

--- gymApplication/gymApplication/Signup.aspx.cs
+++ gymApplication/gymApplication/Signup.aspx.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Configuration;
 
 namespace gymApplication
 {
@@ -11,7 +13,17 @@ namespace gymApplication
     {
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            if (IsPostBack) {
+                SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+                connection.Open();
+                string checkEmail = "SELECT COUNT(*) FROM Users WHERE UserEmail = '" + Email.Text + "'";
+                SqlCommand command = new SqlCommand(checkEmail, connection);
+                int temp = Convert.ToInt32(command.ExecuteScalar().ToString());
+                if (temp == 1) {
+                    Response.Write("User already Exists");
+                }
+                connection.Close();
+            }
         }
 
         protected void Button1_Click(object sender, EventArgs e)
@@ -20,7 +32,29 @@ namespace gymApplication
             if (IsPostBack&& Page.IsValid)
       
             {
-                Response.Redirect("SignupConfirmation.aspx?response=Success");
+                try
+                {
+                    SqlConnection connection = new SqlConnection(ConfigurationManager.ConnectionStrings["RegistrationConnectionString"].ConnectionString);
+                    connection.Open();
+                    string insertQuery = "INSERT INTO Users (UserEmail, UserName, UserPassword) VALUES(@Email,@UserName,@Password)";
+
+                    SqlCommand command = new SqlCommand(insertQuery, connection);
+                    command.Parameters.AddWithValue("@Email", Email.Text);
+                    command.Parameters.AddWithValue("@UserName", Username.Text);
+                    command.Parameters.AddWithValue("@Password", Password.Text);
+
+                    command.ExecuteNonQuery();//Execute Query
+                    Response.Redirect("login.aspx");
+                    Response.Write("Registration Successfull");
+
+                    connection.Close();
+                }
+                catch (Exception )
+                {
+
+                    Response.Write("Ooops something went wrong...");
+                }
+                
             }
         }
     }
---
+++ gymApplication/gymApplication/tempGridview.aspx
@@ -0,0 +1,24 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="tempGridview.aspx.cs" Inherits="gymApplication.tempGridview" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div>
+        	<asp:SqlDataSource ID="RegistrationDataSource" runat="server" ConnectionString="<%$ ConnectionStrings:RegistrationConnectionString %>" SelectCommand="SELECT * FROM [Users]"></asp:SqlDataSource>
+			<asp:GridView ID="GridView1" runat="server" AllowPaging="True" AllowSorting="True" AutoGenerateColumns="False" DataKeyNames="UserEmail" DataSourceID="RegistrationDataSource" Height="152px" Width="455px">
+				<Columns>
+					<asp:CommandField ShowSelectButton="True" />
+					<asp:BoundField DataField="UserEmail" HeaderText="UserEmail" ReadOnly="True" SortExpression="UserEmail" />
+					<asp:BoundField DataField="UserName" HeaderText="UserName" SortExpression="UserName" />
+					<asp:BoundField DataField="UserPassword" HeaderText="UserPassword" SortExpression="UserPassword" />
+				</Columns>
+			</asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
