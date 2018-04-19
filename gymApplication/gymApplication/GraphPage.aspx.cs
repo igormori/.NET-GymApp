3292138312ee355b118ea6ef57acee8fd4934c5c
Dindyal Mursingh <dindyal.mursingh@gmail.com>
2018-04-19 18:04:25 -0400

    loading checkboxes into database
    Co-Authored-By: Sherwayne W. <sherwaynew@users.noreply.github.com>

--- gymApplication/gymApplication/GraphPage.aspx.cs
+++ gymApplication/gymApplication/GraphPage.aspx.cs
@@ -56,21 +56,33 @@ namespace gymApplication
                 using (SqlConnection connection = new SqlConnection(hashed.constring))
                 {
                     connection.Open();
-                    string insertQuery = "INSERT INTO GraphTable (Sunday, Monday, Tuesday, Wednesday, Thrursday, Friday, Saturday) VALUES(@S,@M,@T,@W,@Th,@F,@Sa)";
+                    string insertQuery = "INSERT INTO GraphTable (days_of_the_week,completed) VALUES(@Sun, @Sunval)";
+                    string insertQuery = "INSERT INTO GraphTable (days_of_the_week,completed) VALUES(@Sun, @Sunval)";
+                    string insertQuery = "INSERT INTO GraphTable (days_of_the_week,completed) VALUES(@Sun, @Sunval)";
+                    string insertQuery = "INSERT INTO GraphTable (days_of_the_week,completed) VALUES(@Sun, @Sunval)";
+                    string insertQuery = "INSERT INTO GraphTable (days_of_the_week,completed) VALUES(@Sun, @Sunval)";
+                    string insertQuery = "INSERT INTO GraphTable (days_of_the_week,completed) VALUES(@Sun, @Sunval)";
+                    string insertQuery = "INSERT INTO GraphTable (days_of_the_week,completed) VALUES(@Sun, @Sunval)";
 
                     SqlCommand command = new SqlCommand(insertQuery, connection);
-                    command.Parameters.AddWithValue("@S", CheckBox1.Checked);
-                    command.Parameters.AddWithValue("@M", CheckBox2.Checked);
-                    command.Parameters.AddWithValue("@T", CheckBox3.Checked);
-                    command.Parameters.AddWithValue("@W", CheckBox4.Checked);
-                    command.Parameters.AddWithValue("@Th", CheckBox5.Checked);
-                    command.Parameters.AddWithValue("@F", CheckBox6.Checked);
-                    command.Parameters.AddWithValue("@Sa", CheckBox7.Checked);
-
+                    command.Parameters.AddWithValue("@Sun", 0);
+                    command.Parameters.AddWithValue("@Sunval", CheckBox1.Checked);
+                    command.Parameters.AddWithValue("@Mon", 1);
+                    command.Parameters.AddWithValue("@Monval", CheckBox1.Checked);
+                    command.Parameters.AddWithValue("@Tue", 2);
+                    command.Parameters.AddWithValue("@Tueval", CheckBox1.Checked);
+                    command.Parameters.AddWithValue("@Wed", 3);
+                    command.Parameters.AddWithValue("@Wedval", CheckBox1.Checked);
+                    command.Parameters.AddWithValue("@Thur", 4);
+                    command.Parameters.AddWithValue("@Thurval", CheckBox1.Checked);
+                    command.Parameters.AddWithValue("@Fri", 5);
+                    command.Parameters.AddWithValue("@Frival", CheckBox1.Checked);
+                    command.Parameters.AddWithValue("@Sat", 6);
+                    command.Parameters.AddWithValue("@Satval", CheckBox1.Checked);
                     command.ExecuteNonQuery();//Execute Query
                 }
 
-                Response.Redirect("Home.aspx");
+                
                 results.Text = count.ToString();
 
             }
